dcda126327189860963eef27c587da1ff8dc559d
Andrew <and_0@bk.ru>
2022-03-20 13:44:47 +0400

    Сделал ДЗ 9

---
+++ Task_067/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Показать натуральные числа от N до 1, N задано.
+
+Console.Clear();
+
+Console.Write("Задайте число: ");
+int n = int.Parse(Console.ReadLine());
+
+int NaturalNumber(int num)
+{
+    if (num != 0)
+    {
+        Console.Write($"{num} ");
+        NaturalNumber(num-1);
+    }
+    return num;
+}
+NaturalNumber(n);
---
+++ Task_069/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 69: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму элементов в промежутке от M до N.
+
+Console.Clear();
+
+Console.Write("M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("N: ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+int NaturalNumber(int m, int n)
+{
+    if (m < n)
+    {
+        sum += m;
+        NaturalNumber(m + 1, n);
+        return sum + n;
+    }
+    else if (m > n)
+    {
+        sum += n;
+        NaturalNumber(m, n + 1);
+        return sum + m;
+    }
+    else return 0;
+}
+Console.WriteLine($"\nСумма элементов от M до N равна: {NaturalNumber(m, n)}\n");
